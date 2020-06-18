3ad015374056edcf381d18fe4b3c7ed8fe2a507b
josephexequiel <52370507+josephexequiel@users.noreply.github.com>
2020-06-18 22:50:20 +0800

    Update csv_filter.cpp

--- csv_filter.cpp
+++ csv_filter.cpp
@@ -10,7 +10,7 @@
 
 void insert(std::map<std::string, int> &map1, std::string word)
 {
-     if(map1.find(word) != map1.end())
+     if (map1.find(word) != map1.end())
           map1[word]++;
      else
           map1.insert(std::map<std::string,int>::value_type(word,1));
@@ -18,7 +18,7 @@ void insert(std::map<std::string, int> &map1, std::string word)
 
 int main () {
      std::string line;
-     std::ifstream myfile ("jobs.csv");
+     std::ifstream myfile ("filtered_jobs.csv");
      if (myfile.is_open())
      {
           std::map<std::string, int> myMap;
@@ -30,7 +30,8 @@ int main () {
                     std::istream_iterator<std::string> begin(ss);
                     std::istream_iterator<std::string> end;
                     std::vector<std::string> vstrings(begin, end);
-                    for(std::vector<std::string>::const_iterator i = vstrings.begin(); i != vstrings.end(); ++i) {
+                    for(std::vector<std::string>::const_iterator i = vstrings.begin(); i != vstrings.end(); ++i)
+                    {
                          std::string check_word = *i;
                          check_word.erase(std::remove_if(check_word.begin(), check_word.end(), [](char c) { return !std::isalnum(c); }), check_word.end());
                          std::for_each(check_word.begin(), check_word.end(), [](char & c) {c = ::tolower(c);});
@@ -40,9 +41,13 @@ int main () {
           }
 
           for (auto x : myMap)
+          {
                std::cout << "Word: " << x.first << "\tCount: " << x.second << std::endl;
+          }
+
           myfile.close();
      }
      else std::cout << "Unable to open file"; 
      return 0;
 }
+
